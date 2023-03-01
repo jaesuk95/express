afdbd4b9a3bf83e7cb6b047d8563d9a587849899
Jaesuk <jaesuk95@outlook.com>
2023-03-01 19:24:29 +0900

    library changes

---
+++ dist/app.model.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Cat = void 0;
+exports.Cat = [
+    {
+        id: '1',
+        name: 'blue',
+        age: 8,
+        species: 'Russian Blue',
+        isCute: true,
+        friends: ['2,3']
+    }, {
+        id: '2',
+        name: 'some',
+        age: 4,
+        species: 'Sphynx cat',
+        isCute: true,
+        friends: ['1']
+    }, {
+        id: '3',
+        name: 'munch',
+        age: 3,
+        species: 'Munchkin',
+        isCute: true,
+        friends: []
+    }, {
+        id: '4',
+        name: '옆집고양이',
+        age: 3,
+        species: '옆집',
+        isCute: false,
+        friends: []
+    }
+];
+//# sourceMappingURL=app.model.js.map

04695f63290b71bde1a35d6df3d4515bad416278
Jaesuk <jaesuk95@outlook.com>
2023-03-01 17:04:36 +0900

    NestJS 시작 (packageJson, typeScript config 설정)

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+console.log("hello world")
+console.log("hello world")
+
+// npm install --global nodemon
+// npm install typescript --save-dev
+// npm install --save-dev prettier
+// npx prettier --writen
+// npm install express
+// npm install typescript
+// npx tsc --init
+// npx tsc
+// npm install --save-dev @types/node <- a package that provides TypeScript definitions for Node.js.
+// tsc-watch <- 파일을 계속 지켜보고 있는것이다 , 계속 compile 해준다

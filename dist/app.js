8e011e824e53c4e85834da4cd653041b1565d34e
Jaesuk <jaesuk95@outlook.com>
2023-03-01 17:20:42 +0900

    express 시작

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,13 @@
 "use strict";
-console.log("hello world");
-console.log("hello world");
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var app = express();
+var port = 8000;
+console.log("server is started at port ".concat(port));
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+app.listen(port, function () {
+    console.log("Example app listenting at http://localhost:".concat(port));
+});
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,19 @@
-console.log("hello world")
-console.log("hello world")
+// const express = require('express')
+import * as express from "express";
+
+const app: express.Express = express();
+// const app: express.Application = express();  // 이렇게 해도 됨
+const port: number = 8000;
+
+console.log(`server is started at port ${port}`)
+
+app.get('/', (req,res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listenting at http://localhost:${port}`)
+})
 
 // npm install --global nodemon
 // npm install typescript --save-dev
@@ -11,3 +25,12 @@ console.log("hello world")
 // npx tsc
 // npm install --save-dev @types/node <- a package that provides TypeScript definitions for Node.js.
 // tsc-watch <- 파일을 계속 지켜보고 있는것이다 , 계속 compile 해준다
+
+// npm run start:dev
+// typeScript 를 노드 위에서 사용법
+
+// dependencies 와 devDependencies 의 차이점은 데브 같은 경우는 개발할 때 올려두고, 실제 productionServer 에서는 dependencies
+
+// 추가적으로 js 사용하는 것이 아닌 typeScript 를 사용하기 때문에 추가적으로 설치할게 있다
+// npm i @types/express -D
+// 데브 디펜던시에 올려둔다, package.json 에서 start : node dist/app.js 으로 시작해줄 건데 여기서는 노드로 js 를 시작한다, 컴파일된 파일 위에서 개발을 하기 때문에 데브에 붙인다

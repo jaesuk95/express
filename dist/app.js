e40dd470a1caec4c28ea8d6e43bc907910eadc9e
Jaesuk <jaesuk95@outlook.com>
2023-03-02 15:40:20 +0900

    서버 싱글톤 인스턴스로 만들어 실행시키기

--- dist/app.js
+++ dist/app.js
@@ -5,22 +5,42 @@ var cats_route_1 = require("./cats/cats.route");
 var app = express();
 var port = 8000;
 console.log("server is started at port ".concat(port));
-app.listen(port, function () {
-    console.log("App listening at http://localhost:".concat(port));
-});
-app.use(express.json());
-app.use(function (req, res, next) {
-    console.log("middleware has been processed by request ".concat(req.rawHeaders[3]));
-    next();
-});
-app.use(cats_route_1.default);
+var Server = (function () {
+    function Server() {
+        var app = express();
+        this.app = app;
+    }
+    Server.prototype.setRoute = function () {
+        this.app.use(cats_route_1.default);
+    };
+    Server.prototype.setMiddleware = function () {
+        this.app.use(function (req, res, next) {
+            console.log("middleware has been processed by request ".concat(req.rawHeaders[3]));
+            next();
+        });
+        this.app.use(express.json());
+        this.setRoute();
+        this.app.use(function (req, res, next) {
+            var url = req.url;
+            console.log("request ".concat(url, " has not been found"));
+            res.send({ error: '404 not found error' });
+        });
+    };
+    Server.prototype.listen = function () {
+        this.setMiddleware();
+        this.app.listen(port, function () {
+            console.log("App listening at http://localhost:".concat(port));
+        });
+    };
+    return Server;
+}());
+function init() {
+    var server = new Server();
+    server.listen();
+}
+init();
 app.get('/hello-world', function (req, res) {
     console.log(req);
     res.send('Hello World!');
-});
-app.use(function (req, res, next) {
-    var url = req.url;
-    console.log("request ".concat(url, " has not been found"));
-    res.send({ error: '404 not found error' });
 });
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -8,26 +8,66 @@ const port: number = 8000;
 
 console.log(`server is started at port ${port}`)
 
-app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`)
-})
 
-// express 에서는 바디에 json 을 읽어 올 수 없어서, middleware 에서 추가하도록 한다
-// json middleware
-app.use(express.json());
 
-// tutorial 1
-// middleware
-// 주의: middleware 는 맨 앞에 있어야 한다, 끝에 있으면 안 읽음. 순서가 중요하다
+// 싱글톤 패턴
+// 객체의 인스턴스가 오직 한개만 생성되게 패턴이다
+// 예를 들어, 클래스가 하나 인스턴스로 찍어 내는
+// 메모리 방지를 방지할 수 있다.
+// 여기서는 app 이다
+// 다른 클래스의 인스턴스들이 접근하여 사용할 수 있다.
+class Server {
+    public app: express.Application
 
-// logging middleware
-app.use((req,res,next)=> {
-    console.log(`middleware has been processed by request ${req.rawHeaders[3]}`)
-    next();
-})
+    constructor() {
+        const app: express.Application = express();
+        this.app = app;
+    }
 
-// cats.router.ts 를 걸친다
-app.use(catsRouter);
+
+    // 각각 라우터들을 분리할 수 있다.
+    private setRoute() {
+        // cats.router.ts 를 걸친다
+        this.app.use(catsRouter);
+    }
+
+    private setMiddleware() {
+
+        // logging middleware
+        this.app.use((req,res,next)=> {
+            console.log(`middleware has been processed by request ${req.rawHeaders[3]}`)
+            next();
+        })
+
+        // express 에서는 바디에 json 을 읽어 올 수 없어서, middleware 에서 추가하도록 한다
+        this.app.use(express.json());
+
+        this.setRoute();
+
+        // 잘못된 uri request 대체하는 방식
+        // 404 middleware
+        this.app.use((req,res,next)=> {
+            const url = req.url;
+            console.log(`request ${url} has not been found`)
+            res.send({error: '404 not found error'});
+        })
+    }
+
+    public listen() {
+        this.setMiddleware();
+        this.app.listen(port, () => {
+            console.log(`App listening at http://localhost:${port}`)
+        })
+    }
+}
+
+// 서버가 하나의 인스턴스를 찍혀서, 싱글톤 인스턴스 하나가 서버를 실행시킨다.
+function init() {
+    const server = new Server();
+    server.listen();
+}
+
+init();
 
 
 // 밑에 있는 것을 router 라고 부른다
@@ -39,13 +79,7 @@ app.get('/hello-world', (req,res) => {
 
 
 
-// 잘못된 uri request 대체하는 방식
-// 404 middleware
-app.use((req,res,next)=> {
-    const url = req.url;
-    console.log(`request ${url} has not been found`)
-    res.send({error: '404 not found error'});
-})
+
 
 
 
